46044d0ef952caefa27ebcdd8703502c80cd3b19
Benjamin SICARD <ext-benjamin.sicard@numias.com>
2023-07-07 17:52:51 +0200

    fix: wip

--- tap_aws_cost_explorer/streams.py
+++ tap_aws_cost_explorer/streams.py
@@ -32,11 +32,11 @@ class CostAndUsageWithResourcesStream(AWSCostExplorerStream):
 
     def get_records(self, context: Optional[dict]) -> Iterable[dict]:
         """Return a generator of row-type dictionary objects."""
-        next_page = True
+        next_page_token = None
         start_date = self.get_starting_timestamp(context)
         end_date = self._get_end_date()
 
-        while next_page:
+        while True:
             response = self.conn.get_cost_and_usage(
                 TimePeriod={
                     'Start': start_date.strftime("%Y-%m-%d"),
@@ -54,8 +54,9 @@ class CostAndUsageWithResourcesStream(AWSCostExplorerStream):
                         'Key': 'SERVICE'
                     },
                 ],
+                NextPageToken=next_page_token
             )
-            next_page = response.get("NextPageToken")
+            next_page_token = response.get("NextPageToken")
 
             results_by_time = response.get("ResultsByTime", [])
 
@@ -63,18 +64,7 @@ class CostAndUsageWithResourcesStream(AWSCostExplorerStream):
 
             for row in results_by_time:
               time_period = row.get("TimePeriod", {})
-              # total = row.get("Total", {})
               groups = row.get("Groups", [])
-
-              # for k, v in total.items():
-              #     yield {
-              #         "time_period_start": time_period.get("Start"),
-              #         "time_period_end": time_period.get("End"),
-              #         "group_keys": None,
-              #         "metric_name": k,
-              #         "amount": v.get("Amount"),
-              #         "amount_unit": v.get("Unit")
-              #     }
 
               for group in groups:
                   keys = group.get("Keys", [])
@@ -89,3 +79,6 @@ class CostAndUsageWithResourcesStream(AWSCostExplorerStream):
                           "amount": v.get("Amount"),
                           "amount_unit": v.get("Unit")
                       }
+
+            if not next_page_token:
+              break
